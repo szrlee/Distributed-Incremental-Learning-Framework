8564df767aa69a5f3bf5a7818f5599e499809435
Richard <szrlee@gmail.com>
2018-12-05 14:34:35 +0800

    change default argument

--- src/main.py
+++ src/main.py
@@ -12,11 +12,11 @@ parser.add_argument('--approach', default='', type=str, required=True, \
     choices=['lwf', 'joint_train', 'fine_tuning', 'gem'], help='(default=%(default)s)')
 parser.add_argument('--seed', type=int, default=0, help='(default=%(default)d)')
 parser.add_argument('--lr', default=0.01, type=float, required=False, help='(default=%(default)f)')
-parser.add_argument('--epochs', default=1, type=int, required=False, help='(default=%(default)d)')
+parser.add_argument('--epochs', default=5, type=int, required=False, help='(default=%(default)d)')
 parser.add_argument('--batch_size', default=512, type=int, required=False, help='(default=%(default)d)')
 parser.add_argument('--momentum', default=0.9, type=float, required=False, help='(default=%(default)f)')
 parser.add_argument('--weight_decay', default=0.0005, type=float, required=False, help='(default=%(default)f)')
-parser.add_argument('--print_freq', default=50, type=int, required=False, help='(default=%(default)d)')
+parser.add_argument('--print_freq', default=5, type=int, required=False, help='(default=%(default)d)')
 
 parser.add_argument('--memory_size', default=None, type=int, required=False, help='(default=%(default)d)')
 parser.add_argument('--memory_mini_batch_size', default=None, type=int, required=False, help='(default=%(default)d)')
