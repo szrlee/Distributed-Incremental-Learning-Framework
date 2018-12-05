41f6df597f66ade5cae624bf7c1aa8084f2455d2
szrlee <szrlee@gmail.com>
2018-12-05 14:50:25 +0000

    modify script name

--- src/train.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-now=$(date +"%Y%m%d_%H%M%S")
-python main.py --experiment=voc --approach=joint_train \
-2>&1|tee ../res/log/voc-joint_train-$now.log 
-
--- src/train_finetune.sh
+++ src/train_finetune.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 now=$(date +"%Y%m%d_%H%M%S")
-python main.py --experiment=voc --approach=fine_tuning --epochs 20  \
+python main.py --experiment=voc --approach=fine_tuning --epochs 10  \
 2>&1|tee ../res/log/voc-fine_tuning-$now.log 
 
