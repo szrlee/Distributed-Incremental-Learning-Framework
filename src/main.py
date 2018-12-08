afbebe6302bc4a3242d6c035c48fca8fae99b1f0
Richard <szrlee@gmail.com>
2018-12-08 18:17:55 +0800

    temporally deavtivate gpm

--- src/main.py
+++ src/main.py
@@ -40,8 +40,8 @@ from dataloaders import voc
 from approaches import joint_train
 from approaches import fine_tune
 from approaches import ml_lwf
-from approaches import gpm
-#from approaches import ml_lwf_gpm
+# from approaches import gpm
+# from approaches import ml_lwf_gpm
 
 # Args -- Network
 from networks import resnet
